f7836e477ffb9817858d13f07989c62c661a36eb
Heverton <heverton@gmail.com>
2025-11-10 15:45:29 -0300

    User Role model

---
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
@@ -0,0 +1,41 @@
+package com.devsuperior.demo.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+
+    public Role() {
+
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Role role)) return false;
+
+        return Objects.equals(authority, role.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(authority);
+    }
+}
