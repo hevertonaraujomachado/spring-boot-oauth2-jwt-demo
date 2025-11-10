c09de1ecd1833b30cd6730e3696c76597ac4a95e
Heverton <heverton@gmail.com>
2025-11-10 19:28:43 -0300

    BCrypt password

--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$j/Xispclq8/ql6EJq5wosuIB0/JkZ/bWe5S3HzJmeIGQcvc.SIuJa');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$j/Xispclq8/ql6EJq5wosuIB0/JkZ/bWe5S3HzJmeIGQcvc.SIuJa');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
